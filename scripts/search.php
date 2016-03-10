ef2d185ffe662c1d3fc824284efc4e98c3ddbff2
jeyfost <foster_andrew@tut.by>
2016-03-10 10:28:30 +0300

    Исправлен поиск по сайту

--- scripts/search.php
+++ scripts/search.php
@@ -4,13 +4,13 @@
 	
 	include('connect.php');
 	
-	if(!empty($_POST['searchQuery']) and $_POST['searchQuery'] != iconv ('utf-8', 'windows-1251', 'Поиск...'))
+	if(!empty($_POST['searchQuery']) and $_POST['searchQuery'] != iconv('utf-8', 'windows-1251', 'Поиск...'))
 	{
-		$quantity_result = $mysqli->query("SELECT COUNT(id) FROM catalogue_new WHERE name LIKE '%".trim(stripslashes(htmlspecialchars($_POST['searchQuery'])))."%' AND code LIKE '%".trim(stripslashes(htmlspecialchars($_POST['searchQuery'])))."%'");
+		$quantity_result = $mysqli->query("SELECT COUNT(id) FROM catalogue_new WHERE name LIKE '%".$_POST['searchQuery']."%' AND code LIKE '%".$_POST['searchQuery']."%'");
 		$quantity = $quantity_result->fetch_array(MYSQLI_NUM);
 		
 		$_SESSION['quantity'] = $quantity[0];
-		$_SESSION['query'] = trim(stripslashes(htmlspecialchars($_POST['searchQuery'])));
+		$_SESSION['query'] = $_POST['searchQuery'];
 		
 		header("Location: ../search.php?p=1");
 	}
--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
 	
 	session_start();
-	
+
 	if(empty($_SESSION['query']))
 	{
 		header("Location: index.php");
