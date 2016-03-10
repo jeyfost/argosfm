2b48a5ed0a76fa757d789acde739d410fd42fb22
jeyfost <foster_andrew@tut.by>
2016-03-10 11:48:42 +0300

    Исправлена передача переменных сессии в поиске

--- scripts/search.php
+++ scripts/search.php
@@ -6,12 +6,15 @@
 	
 	if(!empty($_POST['searchQuery']) and $_POST['searchQuery'] != iconv('utf-8', 'windows-1251', 'Поиск...'))
 	{
-		$quantity_result = $mysqli->query("SELECT COUNT(id) FROM catalogue_new WHERE name LIKE '%".$_POST['searchQuery']."%' AND code LIKE '%".$_POST['searchQuery']."%'");
-		$quantity = $quantity_result->fetch_array(MYSQLI_NUM);
+		$quantityNameResult = $mysqli->query("SELECT COUNT(id) FROM catalogue_new WHERE name LIKE '%".$_POST['searchQuery']."%'");
+		$quantityName = $quantityNameResult->fetch_array(MYSQLI_NUM);
+
+		$quantityCodeResult = $mysqli->query("SELECT COUNT(id) FROM catalogue_new WHERE code LIKE '%".$_POST['searchQuery']."%'");
+		$quantityCode = $quantityCodeResult->fetch_array(MYSQLI_NUM);
 		
-		$_SESSION['quantity'] = $quantity[0];
+		$_SESSION['quantity'] = $quantityName[0] + $quantityCode[0];
 		$_SESSION['query'] = $_POST['searchQuery'];
-		
+
 		header("Location: ../search.php?p=1");
 	}
 	else
